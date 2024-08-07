7b1ada4f68df3950ac35592b9e3e0b7c293618f3
Jesus Elias Alba <jesuseliasalba2000@gmail.com>
2024-08-07 16:33:09 +0200

    maiiiin

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 import { Header } from "./src/components/header/header";
 import { Main } from "./src/components/main/main";
 import { Home } from "./src/pages/home/home";
-import { Profile } from "./src/pages/profile/profile";
 import "./style.css";
 
 Main();
-// Home();
-Profile();
+Home();
