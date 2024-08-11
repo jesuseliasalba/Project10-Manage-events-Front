17e36a9dc912493249005307950579f0596a13c5
Jesus Elias Alba <jesuseliasalba2000@gmail.com>
2024-08-11 22:04:50 +0200

    Finished project (for the moment)

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 import { Header } from "./src/components/header/header";
 import { Main } from "./src/components/main/main";
-import { Events } from "./src/pages/events/events";
 import { Home } from "./src/pages/home/home";
 import "./style.css";
 
 Main();
-// Home();
-Events();
+Home();
